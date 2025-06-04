4e4e3f071c4db2525ec95a46e6f4923aef69bddd
itsishant <modakmania@gmail.com>
2025-06-05 02:13:54 +0530

    added some styles

--- Frontend/components/PhotoDashboard.jsx
+++ Frontend/components/PhotoDashboard.jsx
@@ -1,7 +1,12 @@
 export const PhotoDashboard = () => {
     return (
-        <div className="flex bg-white mt px-10">
-            <div className="flex-1 mt-10">
+        <div className="flex bg-white mt px-10 "  >
+            <div className="flex-1 mt-10 "
+            style={{
+    animation: "slideLeft 1.4s ease-out forwards",
+    opacity: 0,
+    transform: "translate(-100%)"  
+  }}>
                 <h1 className="text-7xl font-Poppin ml-44 mt-28">Your fitness journey</h1>
                 <h1 className="text-5xl ml-74 mt-16 font-Poppin">elevated through 💪🏽</h1>
                 <h1 className="text-7xl font-Poppin ml-51 mt-16 flex space-x-4">
@@ -10,12 +15,20 @@ export const PhotoDashboard = () => {
                  <p className="text-black ">insights</p>
                 </h1>
             </div>
-            <div className="mr-10">
-                <img
-                    src="BAKI HANMA.jpeg"
-                    alt="Baki Hanma"
-                    className="h-[600px] mt-8 mr-28 shadow-lg shadow-violet-700 bg-white rounded-full"
-                />
+            <div className="mr-10"
+                    style={{
+    animation: "slideRight 1.4s ease-out forwards",
+    opacity: 0,
+    transform: "translate(100%)"  
+  }}>
+             <img
+  src="BAKI HANMA.jpeg"
+  alt="Baki Hanma"
+  className="h-[600px] mt-8 mr-28 rounded-full shadow-lg shadow-violet-700 bg-white border-4 border-violet-700"
+  style={{ animation: "borderPulse 2s infinite alternate" }}
+/>
+
+
             </div>
         </div>
     );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,14 +1,18 @@
+// tfrontend/tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+      keyframes: {
+        slideIn: {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        slideIn: "slideIn 1s ease-out forwards",
       },
     },
   },
   plugins: [],
-}
+};
