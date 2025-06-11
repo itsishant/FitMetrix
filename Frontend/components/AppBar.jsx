df87322f93b1e9e82761b107b4eebed6c441f0fd
itsishant <modakmania@gmail.com>
2025-06-12 00:00:07 +0530

    cahnges

--- Frontend/components/AppBar.jsx
+++ Frontend/components/AppBar.jsx
@@ -6,7 +6,8 @@ export const AppBar = () => {
     return(
         <div className="flex justify-between items-center h-22 bg-white-600 shadow-2xl border-3 border-violet-600 rounded-4xl mt-4 ml-2 mr-2 shadow--100">
             <div className="m-6 text-5xl flex space-x-2">
-                <p className="font-Poppin">Fit</p> 
+            
+                <p className="font-Poppin"> Fit</p> 
                 <p className="text-violet-700 font-Poppin"> Metrix</p>
             </div>
 
@@ -18,14 +19,14 @@ export const AppBar = () => {
                    <button className="cursor-pointer  hover:bg-violet-400 p-2 rounded-2xl "  onClick={() => {
                     setTimeout(() => {
                         navigate("/dashboard/workout");
-                    },1000)
+                    },)
                    }}>Workouts</button>
                 </div>
                 <div className="text-center text-xl font-Poppin transition delay-1 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
                      <button className="cursor-pointer  hover:bg-violet-400  p-2 rounded-2xl " onClick={() => {
                         setTimeout(() => {
                             navigate("/dashboard/dietplan")
-                        },1000)
+                        },)
                      }} >Diet Plan</button>
                 </div>
                 <div className="text-center text-xl font-Poppin transition delay-1 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
--- Frontend/pages/Workout.jsx
+++ Frontend/pages/Workout.jsx
@@ -1,10 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+
 export const Workout = () => {
 
-    return(
-        <div>
-            hello beta 
-        </div>
-    )
+  return (
+    <div>
+  <div className="flex justify-center items-center mt-12 space-x-4">
+    <FontAwesomeIcon icon={faDumbbell} className="text-6xl rotate-105 vibrate" />
+    <p className="font-Poppin text-6xl text-black mt-4 bg-violet-500 rounded-full inline-block px-6 py-3">
+      Workouts
+    </p>
+    <FontAwesomeIcon icon={faDumbbell} className="text-6xl rotate-75 vibrate" />
+  </div>
+  <div className='flex '>
+    <input type='text' className='border-2 w-[50%] ml-[25%] rounded-4xl p-4 mt-8'></input>
+  </div>
+  </div>
+);
+
 }
 
 
