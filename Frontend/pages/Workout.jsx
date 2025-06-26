495dc123d4ba28f33b7de70ca9412cc591da9c49
itsishant <modakmania@gmail.com>
2025-06-27 03:18:34 +0530

    workout data

--- Frontend/pages/Workout.jsx
+++ Frontend/pages/Workout.jsx
@@ -1,19 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { AppBar } from '../components/AppBar';
+import { useState } from 'react';
+import { workouts } from '../src/assets/workoutData';
 
 export const Workout = () => {
 
-   const workouts = [
-  { name: "Incline Push-Ups", image: "/public/Inclined-pushup.gif", descriptions: "A beginner-friendly push-up variation that targets the chest with reduced resistance." },
-  { name: "Overhead Squat", image: "/public/squats.gif", descriptions: "A full-body squat performed while holding a barbell overhead, improving strength, stability, and mobility." },
-  { name: "Seated Barbell Shoulder Press", image: "/public/chest.gif", descriptions: "An upper-body strength exercise where the lifter presses a barbell overhead while seated, targeting the shoulders and triceps." },
-  { name: "Dumbbell Bicep Curl", image: "/public/bicep.gif", descriptions: "A classic arm exercise where you lift dumbbells by bending your elbows, isolating and building the biceps."},
-  
-  { name: "Lunges", image: "/images/lunges.jpg" },
-  
-];
+  const [search, setSearch] = useState("");
 
+const filterWorkouts = workouts.filter((work) => {
+     return work.name.toLowerCase().startsWith(search.toLowerCase())
+    })
 
   return (
     <div className='bg-gradient-to-r from-slate-300 to-slate-300 min-h-screen'>
@@ -33,8 +30,15 @@ export const Workout = () => {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search " required />
-        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-violet-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => {
+          setTimeout(() => {
+            setSearch(e.target.value)
+          }, 800)
+        }} placeholder="Search " required />    
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-violet-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Search</button>
+
     </div>
 </form>
 
@@ -43,8 +47,10 @@ export const Workout = () => {
 </div>
  */}
 
+
 <div className="grid grid-cols-1 gap-6 md:grid-cols-3  justify-between p-4 ml-4">
-  {workouts.map((workout, index) => (
+  {filterWorkouts.map((workout, index) => {
+    return(
     <div key={index} className="bg-slate-200 h-[450px] w-[450px] rounded-2xl m-4 relative overflow-hidden shadow-lg hover:scale-105 transition">
       <img src={workout.image} alt={workout.name} className="w-full object-cover h-90 rounded-2xl opacity-85 absolute-0" />
       <div className=" text-center relative cursor-pointer p-1.5 font-bold font-Poppin hover:text-violet-600 text-2xl text-gray-800">{workout.name}</div>
@@ -53,7 +59,7 @@ export const Workout = () => {
       </div>
 
     </div>
-  ))}
+)})}
 </div>
 
 
