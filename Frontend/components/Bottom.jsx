1f88703899c72742bd857e849d1bb3c02496adc3
itsishant <modakmania@gmail.com>
2025-07-10 19:21:24 +0530

    some changes

--- Frontend/components/Bottom.jsx
+++ Frontend/components/Bottom.jsx
@@ -1,4 +1,12 @@
+import { Navigate, useNavigate } from "react-router-dom";
+
 export const Bottom = () => {
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/dashboard/workout")
+  }
+
   return (
     <div className="flex flex-wrap justify-center gap-10 p-6">
       <div className="relative w-[730px] h-[500px]">
@@ -7,7 +15,9 @@ export const Bottom = () => {
           className="w-full h-full object-cover opacity-90 rounded-4xl"
           alt=""
         />
-        <button className="absolute bottom-6 left-6 bg-violet-600 font-Poppin py-3 text-slate-200 cursor-pointer hover:bg-violet-700 px-5 rounded-2xl">
+        <button className="absolute bottom-6 left-6 bg-violet-600 font-Poppin py-3 text-slate-200 cursor-pointer hover:bg-violet-700 px-5 rounded-2xl " onClick={() =>{
+          handleClick();
+        }}>
           Bicep Workouts
         </button>
       </div>
